b8143d4f7b4425d3310eeeb253a7b72c3e55f8e1
Fernandez-17 <crfernandezc@unjbg.edu.pe>
2024-06-26 06:40:53 -0500

    Se actualizo el ejercicio 5: se añadio la funcion de calcular raizes complejas(iamginarias)

--- Ejercicio_5.cpp
+++ Ejercicio_5.cpp
@@ -8,7 +8,7 @@ actualizar las variables correspondientes.*/
 
 using namespace std;
 int main( ){
-	float a,b,c,discriminante,raiz_de_discriminante,raiz_1,raiz_2,raiz_unica;
+	float a,b,c,discriminante,raiz_de_discriminante,raiz_1,raiz_2,raiz_unica,parte_real,parte_imaginaria;
 	cout<<"Ingrese el coeficiente del termino cuadratico: ";
 	cin>>a;
 	cout<<"Ingrese el coeficiente del termino lineal: ";
@@ -18,20 +18,31 @@ int main( ){
 	
 	discriminante=pow(b,2)-(4*a*c);
 	if (discriminante>0){
-	cout<<"La ecuación cuadrática tiene dos raíces reales distintas."<<endl;
-	raiz_de_discriminante=sqrt(discriminante);
-	raiz_1= (-b+raiz_de_discriminante)/(2*a);
-	raiz_2= (-b-raiz_de_discriminante)/(2*a);
-	cout<<"Las raices de su ecuacion cuadratica son: "<<endl;
-	cout<<"Raiz 1: "<<raiz_1<<endl;
-	cout<<"Raiz 2: "<<raiz_2<<endl;		
+		
+	    cout<<"La ecuación cuadrática tiene dos raíces reales distintas."<<endl;
+	    raiz_de_discriminante=sqrt(discriminante);
+	    raiz_1= (-(b)+raiz_de_discriminante)/(2*a);
+	    raiz_2= (-(b)-raiz_de_discriminante)/(2*a);
+	    cout<<"Las raices de su ecuacion cuadratica son: " <<endl;
+	    cout<<"Raiz 1: "<<raiz_1<<endl;
+	    cout<<"Raiz 2: "<<raiz_2<<endl;		
 	} else if(discriminante==0){
-		cout<<"La ecuación cuadrática tiene raíces reales iguales."<<endl;
-		raiz_unica=-b/(2*a);
-		cout<<"La unica raiz de esta ecuacion es: "<<raiz_unica<<endl;
+		cout<<"La ecuación cuadrática tiene raíces reales iguales." <<endl;
+		raiz_unica=-(b)/(2*a);
+		cout<<"La ecuacion dos raices iguales: " <<raiz_unica<<endl;
+		cout<<"Raiz 1: "<< raiz_unica << endl;
+	    cout<<"Raiz 2: "<< raiz_unica << endl;	
 	    } else{
-	    	 cout<<"La ecuación cuadrática tiene raíces complejas (imaginarias)."<<endl;
-			 cout<<"Por el momento el sistema no cuenta con esa funcion."<<endl;
+	    	 cout<<"La ecuación cuadrática tiene raíces complejas (imaginarias)." <<endl;
+	    	 parte_real= -(b)/(2*a);
+	    	 parte_imaginaria=sqrt(-discriminante)/(2*a);
+	    	 cout<<"Parte real de la raiz: "<< parte_real << endl;
+	    	 
+	    	 cout<<"Parte imaginaria de la raiz: "<< parte_imaginaria << endl;
+	    	 
+	    	 cout<<"Raiz 1: "<<parte_real << " + i " << parte_imaginaria << endl;
+	    	 
+	    	 cout<<"Raiz 2: "<<parte_real << " - i " << parte_imaginaria << endl;	 
 	    }
 		   	
 	return 0;
